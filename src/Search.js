abc7b13b606ca3e5839c656ab536d3fcc44b9a49
Lucas Matos <lucaslmatos18@gmail.com>
2023-02-06 14:20:46 -0300

    Adicinando funcionalidade da rota Search - Req 05

--- src/Search.js
+++ src/Search.js
@@ -2,11 +2,47 @@ import React from 'react';
 import Header from './components/Header';
 
 class Search extends React.Component {
+  state = {
+    isSearchBtnDisabled: true,
+    searchValue: '',
+  };
+
+  handleChange = ({ target }) => {
+    const num = 2;
+    this.setState({
+      searchValue: target.value,
+    });
+    if (target.value.length >= num) {
+      this.setState({
+        isSearchBtnDisabled: false,
+      });
+    } else {
+      this.setState({
+        isSearchBtnDisabled: true,
+      });
+    }
+  };
+
   render() {
+    const { isSearchBtnDisabled, searchValue } = this.state;
     return (
       <div>
         <Header />
-        <div data-testid="page-search">Search</div>
+        <div data-testid="page-search">
+          <input
+            type="text"
+            data-testid="search-artist-input"
+            value={ searchValue }
+            onChange={ this.handleChange }
+          />
+          <button
+            data-testid="search-artist-button"
+            disabled={ isSearchBtnDisabled }
+            onClick={ this.onSearchBtnClick }
+          >
+            Entrar
+          </button>
+        </div>
       </div>
     );
   }
